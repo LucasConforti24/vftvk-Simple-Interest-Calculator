726c2800cad35f3983bbacedeb57903c53964db6
LucasConforti24 <86933193+LucasConforti24@users.noreply.github.com>
2021-07-04 21:06:28 -0300

    Update script.js
    updated script

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = p * years * rate /100;
+    var year= new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText="If you deposit "+ p + ", \
+    <br\>at an interest rate of "+ rate + ". %\<br\> You will recieve an amount of "+ interest +" in the year "+ year +".";
+
+}
+
+function updateRate()
+{
+
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
 }
         
